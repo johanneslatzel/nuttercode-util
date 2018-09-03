639eaa54145ca05bf586e5eedffcb5d81af8df8c
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-09-03 10:55:59 +0200

    moved methods to Assurance
    moved methods to Assurance

--- java/de/nuttercode/util/Closeable.java
+++ java/de/nuttercode/util/Closeable.java
@@ -4,14 +4,4 @@ public interface Closeable extends java.io.Closeable {
 
 	boolean isClosed();
 
-	default void assureNotClosed() {
-		if (isClosed())
-			throw new IllegalStateException();
-	}
-
-	default void assureClosed() {
-		if (!isClosed())
-			throw new IllegalStateException();
-	}
-
 }
