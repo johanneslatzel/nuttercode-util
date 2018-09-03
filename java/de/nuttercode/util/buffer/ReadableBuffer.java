ee62faf80f987a0d58ce578079ffc12daed42549
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-09-03 10:49:09 +0200

    docs, new method, removed unused method
    docs, new method, removed unused method

--- java/de/nuttercode/util/buffer/ReadableBuffer.java
+++ java/de/nuttercode/util/buffer/ReadableBuffer.java
@@ -2,41 +2,112 @@ package de.nuttercode.util.buffer;
 
 import java.nio.ByteBuffer;
 
-import de.nuttercode.util.Closeable;
+/**
+ * represents a readable buffer
+ * 
+ * @author Johannes B. Latzel
+ *
+ */
+public interface ReadableBuffer {
 
-public interface ReadableBuffer extends Closeable {
-
+	/**
+	 * returns the next int in the buffer at the current position and forwards the
+	 * position by {@link Integer#BYTES} bytes
+	 * 
+	 * @return next int
+	 */
 	int getInt();
 
+	/**
+	 * returns the next long in the buffer at the current position and forwards the
+	 * position by {@link Long#BYTES} bytes
+	 * 
+	 * @return next long
+	 */
 	long getLong();
 
+	/**
+	 * returns the next float in the buffer at the current position and forwards the
+	 * position by {@link Float#BYTES} bytes
+	 * 
+	 * @return next float
+	 */
 	float getFloat();
 
+	/**
+	 * returns the next double in the buffer at the current position and forwards
+	 * the position by {@link Double#BYTES} bytes
+	 * 
+	 * @return next double
+	 */
 	double getDouble();
 
+	/**
+	 * returns the next String in the buffer at the current position. first getInt()
+	 * is called. the returned determines the size (n) of the following string. the
+	 * position will be forwarded by n + {@link Integer#BYTES} bytes.
+	 * 
+	 * @return next String
+	 */
 	String getString();
 
+	/**
+	 * returns the next char in the buffer at the current position and forwards the
+	 * position by {@link Character#BYTES} bytes
+	 * 
+	 * @return next char
+	 */
 	char getChar();
 
+	/**
+	 * returns the next byte in the buffer at the current position and forwards the
+	 * position by {@link Byte#BYTES} bytes
+	 * 
+	 * @return next byte
+	 */
 	byte getByte();
 
+	/**
+	 * returns the next short in the buffer at the current position and forwards the
+	 * position by {@link Short#BYTES} bytes
+	 * 
+	 * @return next short
+	 */
 	short getShort();
 
+	/**
+	 * reads the next bytes.length bytes from the buffer and writes them into bytes.
+	 * 
+	 * @return bytes
+	 */
 	byte[] getBytes(byte[] bytes);
 
+	/**
+	 * @return number of bytes that can be read from the buffer
+	 */
 	int transferableData();
 
+	/**
+	 * @return true if {@link #transferableData()} > 0
+	 */
 	default boolean hasTransferableData() {
-		assureNotClosed();
 		return transferableData() > 0;
 	}
 
-	default void assureConsumableData(int size) {
-		assureNotClosed();
-		if (transferableData() < size)
-			throw new IllegalStateException();
-	}
-
+	/**
+	 * reads the next {@link ByteBuffer#remaining()} bytes from this buffer and puts
+	 * them into outerBuffer.
+	 * 
+	 * @param outerBuffer
+	 * @return outerBuffer
+	 */
 	ByteBuffer transferDataInto(ByteBuffer outerBuffer);
+
+	/**
+	 * @return a readable view of this buffer
+	 */
+	default ReadableBuffer readableView() {
+		return new ReadableBufferWrapper(this);
+	}
 
 }
