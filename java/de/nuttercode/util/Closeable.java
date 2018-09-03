adc2885e06527b6b4c2f4e9f29325d1d244bc30b
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-09-03 11:02:14 +0200

    added docs
    added docs

--- java/de/nuttercode/util/Closeable.java
+++ java/de/nuttercode/util/Closeable.java
@@ -1,7 +1,17 @@
 package de.nuttercode.util;
 
+/**
+ * extension of {@link java.io.Closeable} interface. provides a method to
+ * determine if an object has been closed or not
+ * 
+ * @author Johannes B. Latzel
+ *
+ */
 public interface Closeable extends java.io.Closeable {
 
+	/**
+	 * @return true if the object has been closed
+	 */
 	boolean isClosed();
 
 }
