9645e05fbf8bfcd6529a62fe14191ecaf17bc102
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-09-03 11:03:10 +0200

    added docs
    added docs

--- java/de/nuttercode/util/Initializable.java
+++ java/de/nuttercode/util/Initializable.java
@@ -1,7 +1,16 @@
 package de.nuttercode.util;
 
+/**
+ * provides a method to determine if an object has been initialized
+ * 
+ * @author Johannes B. Latzel
+ *
+ */
 public interface Initializable {
 
+	/**
+	 * @return true if the object has been initialized
+	 */
 	boolean isInitialized();
 
 }
