1242615e41db9d23ddcbfda42540587d4450bd93
Johannes B. Latzel <johannes.latzel@users.noreply.github.com>
2018-09-03 10:48:12 +0200

    docs, no modeablebuffer, no closeable, use of assurance
    docs, no modeablebuffer, no closeable, use of assurance

--- java/de/nuttercode/util/buffer/DynamicBuffer.java
+++ java/de/nuttercode/util/buffer/DynamicBuffer.java
@@ -2,15 +2,39 @@ package de.nuttercode.util.buffer;
 
 import java.nio.ByteBuffer;
 
-public class DynamicBuffer implements WriteableBuffer, ReadableBuffer, ModeableBuffer {
+import de.nuttercode.util.Assurance;
 
+/**
+ * used when a java.nio.ByteBuffer is needed but the size is unknown. be aware
+ * that this buffer will grow as data is put into it. if the size of the buffer
+ * is known at compile-time then this class should not be used.
+ * 
+ * @author Johannes B. Latzel
+ *
+ */
+public class DynamicBuffer implements WriteableBuffer, ReadableBuffer {
+
+	/**
+	 * default size of the copy buffer in bytes
+	 */
 	private final static int COPY_BUFFER_SIZE = 64;
 
+	/**
+	 * underlying buffer
+	 */
 	private ByteBuffer buffer;
+
+	/**
+	 * used to copy data between buffers
+	 */
 	private final byte[] copyBuffer;
+
 	private BufferMode mode;
+
+	/**
+	 * true if the mode can not be changed
+	 */
 	private boolean isModeLocked;
-	private boolean isClosed;
 
 	public DynamicBuffer(int initialCapacity, boolean isDirect) {
 		if (isDirect)
@@ -20,11 +44,14 @@ public class DynamicBuffer implements WriteableBuffer, ReadableBuffer, ModeableB
 		copyBuffer = new byte[COPY_BUFFER_SIZE];
 		mode = BufferMode.Write;
 		isModeLocked = false;
-		isClosed = false;
 	}
 
+	/**
+	 * assures that at least additionalCapacity is available in the buffer
+	 * 
+	 * @param additionalCapacity
+	 */
 	private void assureCapacity(int additionalCapacity) {
-		assureNotClosed();
 		if (buffer.remaining() >= additionalCapacity)
 			return;
 		int newCapacity = (buffer.capacity() * 3) / 2 + additionalCapacity;
@@ -38,15 +65,47 @@ public class DynamicBuffer implements WriteableBuffer, ReadableBuffer, ModeableB
 		buffer = newBuffer;
 	}
 
+	public void setMode(BufferMode mode) {
+		if (isModeLocked())
+			throw new IllegalStateException();
+		switch (mode) {
+		case Read:
+			Assurance.assureMode(this, BufferMode.Write);
+			this.mode = mode;
+			buffer.flip();
+			this.mode = BufferMode.Read;
+			break;
+		case Write:
+			Assurance.assureMode(this, BufferMode.Read);
+			buffer.clear();
+			this.mode = BufferMode.Write;
+			break;
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public BufferMode getMode() {
+		return mode;
+	}
+
+	public void lockMode() {
+		isModeLocked = true;
+	}
+
+	public boolean isModeLocked() {
+		return isModeLocked;
+	}
+
 	@Override
 	public int transferableData() {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		return buffer.limit() - buffer.position();
 	}
 
 	@Override
 	public ByteBuffer transferDataInto(ByteBuffer outerBuffer) {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		if (outerBuffer.remaining() >= buffer.remaining()) {
 			return outerBuffer.put(buffer);
 		}
@@ -64,35 +123,35 @@ public class DynamicBuffer implements WriteableBuffer, ReadableBuffer, ModeableB
 
 	@Override
 	public void putInt(int i) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		assureCapacity(Integer.BYTES);
 		buffer.putInt(i);
 	}
 
 	@Override
 	public void putLong(long l) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		assureCapacity(Long.BYTES);
 		buffer.putLong(l);
 	}
 
 	@Override
 	public void putFloat(float f) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		assureCapacity(Float.BYTES);
 		buffer.putFloat(f);
 	}
 
 	@Override
 	public void putDouble(double d) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		assureCapacity(Double.BYTES);
 		buffer.putDouble(d);
 	}
 
 	@Override
 	public void putString(String s) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		byte[] bytes = s.getBytes();
 		putInt(bytes.length);
 		putBytes(bytes);
@@ -100,38 +159,38 @@ public class DynamicBuffer implements WriteableBuffer, ReadableBuffer, ModeableB
 
 	@Override
 	public void putChar(char c) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		assureCapacity(Character.BYTES);
 		buffer.putChar(c);
 	}
 
 	@Override
 	public int getInt() {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		return buffer.getInt();
 	}
 
 	@Override
 	public long getLong() {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		return buffer.getLong();
 	}
 
 	@Override
 	public float getFloat() {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		return buffer.getFloat();
 	}
 
 	@Override
 	public double getDouble() {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		return buffer.getDouble();
 	}
 
 	@Override
 	public String getString() {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		if (transferableData() < Integer.BYTES)
 			throw new IllegalStateException();
 		byte[] bytes = new byte[getInt()];
@@ -142,51 +201,51 @@ public class DynamicBuffer implements WriteableBuffer, ReadableBuffer, ModeableB
 
 	@Override
 	public char getChar() {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		return buffer.getChar();
 	}
 
 	@Override
 	public short getShort() {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		return buffer.getShort();
 	}
 
 	public void putClear(int bytes) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		putBytes(new byte[bytes]);
 	}
 
 	@Override
 	public void putByte(byte b) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		assureCapacity(Byte.BYTES);
 		buffer.put(b);
 	}
 
 	@Override
 	public void putBytes(byte[] bytes) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		assureCapacity(bytes.length);
 		buffer.put(bytes);
 	}
 
 	@Override
 	public byte getByte() {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		return buffer.get();
 	}
 
 	@Override
 	public byte[] getBytes(byte[] bytes) {
-		assureMode(BufferMode.Read);
+		Assurance.assureMode(this, BufferMode.Read);
 		buffer.get(bytes);
 		return bytes;
 	}
 
 	@Override
 	public void putByteBuffer(ByteBuffer byteBuffer) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		while (byteBuffer.remaining() >= COPY_BUFFER_SIZE) {
 			byteBuffer.get(copyBuffer);
 			putBytes(copyBuffer);
@@ -200,59 +259,8 @@ public class DynamicBuffer implements WriteableBuffer, ReadableBuffer, ModeableB
 
 	@Override
 	public void putShort(short s) {
-		assureMode(BufferMode.Write);
+		Assurance.assureMode(this, BufferMode.Write);
 		buffer.putShort(s);
-	}
-
-	@Override
-	public void setMode(BufferMode mode) {
-		assureNotClosed();
-		if (isModeLocked())
-			throw new IllegalStateException();
-		switch (mode) {
-		case Read:
-			assureMode(BufferMode.Write);
-			this.mode = mode;
-			buffer.flip();
-			this.mode = BufferMode.Read;
-			break;
-		case Write:
-			assureMode(BufferMode.Read);
-			buffer.clear();
-			this.mode = BufferMode.Write;
-			break;
-		default:
-			throw new IllegalArgumentException();
-		}
-	}
-
-	@Override
-	public BufferMode getMode() {
-		assureNotClosed();
-		return mode;
-	}
-
-	@Override
-	public void lockMode() {
-		assureNotClosed();
-		isModeLocked = true;
-	}
-
-	@Override
-	public boolean isModeLocked() {
-		assureNotClosed();
-		return isModeLocked;
-	}
-
-	@Override
-	public void close() {
-		assureNotClosed();
-		isClosed = true;
-	}
-
-	@Override
-	public boolean isClosed() {
-		return isClosed;
 	}
 
 	@Override
