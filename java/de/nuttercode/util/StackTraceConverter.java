236e4a19ae55fa8642ef943c27c746f5ed04167e
Johannes B. Latzel <34381668+johanneslatzel@users.noreply.github.com>
2018-10-21 12:54:39 +0200

    converts StackTraceElement[] directly
    converts StackTraceElement[] directly

--- java/de/nuttercode/util/StackTraceConverter.java
+++ java/de/nuttercode/util/StackTraceConverter.java
@@ -33,7 +33,20 @@ public final class StackTraceConverter {
 	 */
 	public static @NotNull String getStackTrace(@NotNull Thread thread) {
 		Assurance.assureNotNull(thread);
-		StackTraceElement[] stackTraceArray = thread.getStackTrace();
+		return getStackTrace(thread.getStackTrace());
+	}
+
+	/**
+	 * converts the trace of the given stack to a {@link java.lang.String String}
+	 * 
+	 * @param stackTraceArray
+	 * @return the stack trace - every {@link java.lang.StackTraceElement
+	 *         StackTraceElement} is printed on its own line
+	 * @throws IllegalArgumentException
+	 *             if the thread is equal to null
+	 */
+	public static @NotNull String getStackTrace(@NotNull StackTraceElement[] stackTraceArray) {
+		Assurance.assureNotNull(stackTraceArray);
 		StringBuilder stackTrace = new StringBuilder(stackTraceArray.length * 50);
 		for (int a = 0; a < stackTraceArray.length; a++) {
 			stackTrace.append(stackTraceArray[a].toString());
